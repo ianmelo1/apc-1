6a16f5d892afbbe08fea43b147d8e8c614f6e495
ian melo gonçalves <160737561+ianmelo1@users.noreply.github.com>
2024-05-22 13:45:58 +0000

    correção

--- aulas/aula08/boletim.c
+++ aulas/aula08/boletim.c
@@ -6,12 +6,13 @@ int main(){
   for(int i=0; i<10; i++){
     for(int j=0; j<2; j++){
       printf("Entre com a nota %i do aluno %i: ", j, i);
-      int deu_certo = scanf("%f", &boletim[i][1]);
+      int deu_certo = scanf("%f", &boletim[i][j]);
     }
     boletim[i][2] = 0.4f * boletim[i][0] + 0.6f * boletim[i][1];
   }
 
   printf("\n B O L E T I M  D E  N O T A S \n");
+  printf("Nota1 Nota2 Media\n");
   for(int i=0; i<10; i++){
     for(int j=0; j<3; j++){
       printf("%5.1f", boletim [i][j]);
